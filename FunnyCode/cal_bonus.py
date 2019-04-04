0a21a78a812f33dacb0899c47a88d21a12c249d7
javametro <minda1949@sina.com>
2019-04-04 23:54:49 +0800

    modify it adapt to python 3

--- FunnyCode/cal_bonus.py
+++ FunnyCode/cal_bonus.py
@@ -3,7 +3,7 @@
 
 import sys
 
-bonus_num = float(raw_input("Enter your bonus:"))
+bonus_num = float(input("Enter your bonus:"))
 #print bonus_num
 
 tax_base = bonus_num / 12.0
@@ -32,8 +32,7 @@ elif tax_base >= 80000:
 	tax_rate = 0.45
 	remain_num = 13505
 
-#print "Your tax_rate is %f" % tax_rate, "your remain_num is %f" % remain_num
-
 tax_num = 0
 tax_num = bonus_num * tax_rate - remain_num
-print "Your tax_num is %f" % tax_num
+print ("Your tax_num is %f" % tax_num)
+	
