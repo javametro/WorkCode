f18cd72d2dc8909b09e286d6dfb5bb17730085d2
javametro <minda1949@sina.com>
2022-06-30 09:44:19 +0800

    add runCommand project

---
+++ RunCommand/RunCommand/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C schtasks /change /tn HardwareNotifyApp2 /DELAY 0002:00";
+            process.StartInfo = startInfo;
+            process.Start();
+            Console.ReadLine();
+        }
+    }
+}
